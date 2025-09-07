d0673b62041d6cced582c78889fe2deeb21ba91a
doryosi <103187334+doryosi@users.noreply.github.com>
2025-09-07 11:48:28 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# manually install spectecales
+brew install --cask iterm2
+brew install --cask alt-tab
+brew install --cask visual-studio-code
+brew install --cask flameshot
+brew install --cask hiddenbar
+brew install --cask chatgpt
