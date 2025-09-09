2c9828e5ca444e2108b31cf469b84f975947e725
doryosi <103187334+doryosi@users.noreply.github.com>
2025-09-09 16:21:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,36 @@ brew install --cask visual-studio-code
 brew install --cask flameshot
 brew install --cask hiddenbar
 brew install --cask chatgpt
+# Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+
+# Install plugins
+brew install zsh-autosuggestions
+echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+brew install zsh-syntax-highlighting
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+brew install zsh-autocomplete 
+echo "source /opt/homebrew/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+brew install zsh-completions
+#To activate these completions, add the following to your .zshrc:
+#
+#   if type brew &>/dev/null; then
+#     FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+
+#     autoload -Uz compinit
+#     compinit
+#   fi
+
+# You may also need to force rebuild `zcompdump`:
+
+#   rm -f ~/.zcompdump; compinit
+
+# Additionally, if you receive "zsh compinit: insecure directories" warnings when attempting
+# to load these completions, you may need to run these commands:
+
+#   chmod go-w '/opt/homebrew/share'
+#   chmod -R go-w '/opt/homebrew/share/zsh'
